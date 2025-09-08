7f4cf450f577fe8c64e64730a44b6dc1d75e20bb
ViccaCuenco-UCM <maevicca31@gmail.com>
2025-09-08 20:08:00 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,14 @@
+public class FixDebugOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    /*
+     List down the spotted Bugs below:
+     1. Invalid class name with space — it should be `FixDebugOne` instead of `FixDebug One` 
+     2. Incorrect capitalization method name — main instead of Main
+     3. Incorrect parameter declaration — changed `String args` to `String[] args`
+     4. Typo in the word`System` — correct spelling instead of `Systm` to `System`
+     5. Missing semicolon — added `;` after `System.out.println("Hello World!")`
+     */
+    }
+}
